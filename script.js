c8d75069528439fd08bb2b89e5bda956fa1c3fd3
Nathan <pythoniszen@gmail.com>
2023-06-09 16:54:23 -0700

    6/9/23 Add erase button

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 const grid = document.querySelector('#grid');
-
-for (let i = 0; i < 256; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  grid.appendChild(cell);
-}
-
-let cells = document.querySelectorAll('.cell')
 let isMoving = false;
+let eraseMode = false;
 
-cells.forEach(cell => {
+script();
 
-    cell.addEventListener('mousedown', () => {
-        cell.style.backgroundColor = 'black'
-        isMoving = true;
+function script() {
+    for (let i = 0; i < 256; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+
+    let cells = document.querySelectorAll('.cell');
+
+    const eraseModeButton = document.querySelector('#eraseBtn');
+    eraseModeButton.addEventListener('click', () => {
+      if (eraseMode === true) {
+        eraseMode = false;
+        eraseModeButton.style.backgroundColor = '';
+      } else if (eraseMode === false) {
+        eraseMode = true;
+        eraseModeButton.style.backgroundColor = 'rgb(34, 233, 34)';
+      }
     });
+    
 
-    cell.addEventListener('mousemove', () => {
-        if (isMoving) {
-            cell.style.backgroundColor = 'black'
-        }
+    cells.forEach(cell => {
+        cell.addEventListener('mousedown', () => {
+            if (eraseMode === false) {
+                cell.style.backgroundColor = 'black';
+            } else {
+                cell.style.backgroundColor = 'white';
+            }
+            isMoving = true;
+        });
+
+        cell.addEventListener('mousemove', () => {
+            if (isMoving) {
+                if (eraseMode === false) {
+                    cell.style.backgroundColor = 'black';
+                } else {
+                    cell.style.backgroundColor = 'white';
+                }
+            }
+        });
+
+        grid.addEventListener('mouseup', () => {
+            isMoving = false;
+        });
     });
-
-
-    grid.addEventListener('mouseup', () => {
-        isMoving = false;
-    });
-});
-
+}
