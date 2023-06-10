ec7532b91f3ce7634040f94ca5504c2bded9cd8f
Nathan <pythoniszen@gmail.com>
2023-06-09 18:18:10 -0700

    6/9/23 Add grid button and fix bug where grid border does not align

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('#grid');
 let isMoving = false;
 let eraseMode = false;
+let brushColor = 'black';
 
 script();
 
@@ -17,18 +18,46 @@ function script() {
     eraseModeButton.addEventListener('click', () => {
       if (eraseMode === true) {
         eraseMode = false;
-        eraseModeButton.style.backgroundColor = '';
+        eraseModeButton.style.backgroundColor = 'white';
       } else if (eraseMode === false) {
         eraseMode = true;
         eraseModeButton.style.backgroundColor = 'rgb(34, 233, 34)';
       }
     });
     
+    const clearButton = document.querySelector('#clearBtn');
+    clearButton.addEventListener('click', () => {
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'white';
+        });
+    });
 
+    let gridMode = true;
+    const gridButton = document.querySelector('#gridBtn');
+    const sketchPad = document.querySelector('#sketchPad');
+    gridButton.addEventListener('click', () => {
+        if (gridMode === true) {
+            cells.forEach(cell => {
+                cell.style.border = 'none';
+                sketchPad.style.border = 'none';
+                gridButton.style.backgroundColor = 'white';
+            });
+            gridMode = false;
+        } else if (gridMode === false) {
+            cells.forEach(cell => {
+                cell.style.border = '1px solid black';
+                sketchPad.style.border = '1px solid black';
+                gridButton.style.backgroundColor = 'rgb(34, 233, 34)';
+            });
+            gridMode = true;
+        }
+    });
+    
+    //Function draws cells onto canvas and determines which color should be used
     cells.forEach(cell => {
         cell.addEventListener('mousedown', () => {
             if (eraseMode === false) {
-                cell.style.backgroundColor = 'black';
+                cell.style.backgroundColor = brushColor;
             } else {
                 cell.style.backgroundColor = 'white';
             }
@@ -38,7 +67,7 @@ function script() {
         cell.addEventListener('mousemove', () => {
             if (isMoving) {
                 if (eraseMode === false) {
-                    cell.style.backgroundColor = 'black';
+                    cell.style.backgroundColor = brushColor;
                 } else {
                     cell.style.backgroundColor = 'white';
                 }
