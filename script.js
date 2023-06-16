0a6cd6e39ccb4a2e58564ac4e7bb20825c38a803
Nathan <pythoniszen@gmail.com>
2023-06-15 20:04:13 -0700

    6/15/23 Add range slider to set size of grid

--- script.js
+++ script.js
@@ -3,33 +3,26 @@ let isMoving = false;
 let eraseMode = false;
 let brushColor = 'black';
 
+const gridSizeSlider = document.querySelector('#gridSize');
+gridSizeSlider.addEventListener('input', updateGridSize);
+
 script();
 
 function script() {
-    for (let i = 0; i < 256; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        grid.appendChild(cell);
-    }
+    gridSizeSlider.value = 16;
+    updateGridSize();
 
     const colorSelect = document.querySelector('#colorSelect');
     colorSelect.addEventListener('change', () => {
         brushColor = colorSelect.value;
     });
 
-    let cells = document.querySelectorAll('.cell');
-
     const eraseModeButton = document.querySelector('#eraseBtn');
     eraseModeButton.addEventListener('click', () => {
-      if (eraseMode === true) {
-        eraseMode = false;
-        eraseModeButton.style.backgroundColor = 'white';
-      } else if (eraseMode === false) {
-        eraseMode = true;
-        eraseModeButton.style.backgroundColor = 'rgb(34, 233, 34)';
-      }
+        eraseMode = !eraseMode;
+        eraseModeButton.style.backgroundColor = eraseMode ? 'rgb(34, 233, 34)' : 'white';
     });
-    
+
     const clearButton = document.querySelector('#clearBtn');
     clearButton.addEventListener('click', () => {
         cells.forEach(cell => {
@@ -39,43 +32,48 @@ function script() {
 
     let gridMode = true;
     const gridButton = document.querySelector('#gridBtn');
-    const sketchPad = document.querySelector('#sketchPad');
     gridButton.addEventListener('click', () => {
-        if (gridMode === true) {
-            cells.forEach(cell => {
-                cell.style.border = 'none';
-          
-                gridButton.style.backgroundColor = 'white';
-            });
-            gridMode = false;
-        } else if (gridMode === false) {
-            cells.forEach(cell => {
-                cell.style.border = '1px solid black';
-             
-                gridButton.style.backgroundColor = 'rgb(34, 233, 34)';
-            });
-            gridMode = true;
-        }
+        gridMode = !gridMode;
+        cells.forEach(cell => {
+            cell.style.border = gridMode ? '1px solid black' : 'none';
+        });
+        gridButton.style.backgroundColor = gridMode ? 'rgb(34, 233, 34)' : 'white';
     });
-    
-    //Function draws cells onto canvas and determines which color should be used
+}
+
+function updateGridSize() {
+    let newSize = gridSizeSlider.value;
+
+    const gridSizeDisplay = document.querySelector('#gridSizeDisplay');
+    gridSizeDisplay.textContent = `${newSize}x${newSize}`;
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    grid.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+
+    for (let i = 0; i < newSize * newSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+
+    cells = document.querySelectorAll('.cell');
+    addCellEventListeners();
+}
+
+function addCellEventListeners() {
     cells.forEach(cell => {
         cell.addEventListener('mousedown', () => {
-            if (eraseMode === false) {
-                cell.style.backgroundColor = brushColor;
-            } else {
-                cell.style.backgroundColor = 'white';
-            }
+            cell.style.backgroundColor = eraseMode ? 'white' : brushColor;
             isMoving = true;
         });
 
         cell.addEventListener('mousemove', () => {
             if (isMoving) {
-                if (eraseMode === false) {
-                    cell.style.backgroundColor = brushColor;
-                } else {
-                    cell.style.backgroundColor = 'white';
-                }
+                cell.style.backgroundColor = eraseMode ? 'white' : brushColor;
             }
         });
 
